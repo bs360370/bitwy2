8a8b8a4a632c93e9db71953be44dbfe5764cc5eb
bs360370 <bs360370@students.mimuw.edu.pl>
2017-12-28 18:45:55 +0100

    dodana klasa Wsparcie

---
+++ Wsparcie.h
@@ -0,0 +1,16 @@
+//
+// Created by Basia on 28.12.2017.
+//
+
+#ifndef BITWY2_WSPARCIE_H
+#define BITWY2_WSPARCIE_H
+
+
+#include "Oddzial.h"
+
+class Wsparcie : public Oddzial {
+
+};
+
+
+#endif //BITWY2_WSPARCIE_H
--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include "Oddzial.h"
+#include "Wsparcie.h"
 
 int main() {
 
-    Oddzial* x = new Oddzial();
+    Wsparcie* x = new Wsparcie();
 
     std::cout << "Hello, World!" << std::endl;
 
