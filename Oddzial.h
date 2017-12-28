b26df7a499d67f8f7c7ba3c1266dd74ea4c8ac52
bs360370 <bs360370@students.mimuw.edu.pl>
2017-12-28 18:32:07 +0100

    init

---
+++ Oddzial.h
@@ -0,0 +1,14 @@
+//
+// Created by Basia on 28.12.2017.
+//
+
+#ifndef BITWY2_ODDZIAL_H
+#define BITWY2_ODDZIAL_H
+
+
+class Oddzial {
+
+};
+
+
+#endif //BITWY2_ODDZIAL_H
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Oddzial.h"
+
+int main() {
+
+    Oddzial* x = new Oddzial();
+
+    std::cout << "Hello, World!" << std::endl;
+
+    delete (x);
+
+    return 0;
+}
